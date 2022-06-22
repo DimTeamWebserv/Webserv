ac979ff6025e05a80e36239a930eb00789fd06cd
DokyeongKim <charlie@Charlie-Mac.local>
2022-06-22 16:32:58 +0900

    [feat] : making response msg

--- srcs/response.cpp
+++ srcs/response.cpp
@@ -1,50 +1,107 @@
-#include <iostream>
 #include <map>
 #include <vector>
+#include <fstream>
+#include "../includes/Response.hpp"
 
-int main(void)
+static std::string set_url(Request request, Response response)
 {
-	std::map<int, std::string> status_code;
-
-	status_code.insert(std::make_pair(100, "Continue"));						//
-	status_code.insert(std::make_pair(101, "Switching Protocols"));				//
-	status_code.insert(std::make_pair(200, "OK"));								// 정상
-	status_code.insert(std::make_pair(201, "Created"));							//
-	status_code.insert(std::make_pair(202, "Accepted"));						//
-	status_code.insert(std::make_pair(203, "Non-Authoritative Information"));	//
-	status_code.insert(std::make_pair(204, "No Content"));						//
-	status_code.insert(std::make_pair(205, "Reset Content"));					//
-	status_code.insert(std::make_pair(206, "Partial Content"));					//
-	status_code.insert(std::make_pair(300, "Multiple Choices"));				//
-	status_code.insert(std::make_pair(301, "Moved Permanently"));				//
-	status_code.insert(std::make_pair(302, "Found"));							//
-	status_code.insert(std::make_pair(303, "See Other"));						//
-	status_code.insert(std::make_pair(304, "Not Modified"));					//
-	status_code.insert(std::make_pair(305, "Use Proxy"));						//
-	status_code.insert(std::make_pair(307, "Temporary Redirect"));				//
-	status_code.insert(std::make_pair(400, "Bad Request"));						//
-	status_code.insert(std::make_pair(401, "Unauthorized"));					//
-	status_code.insert(std::make_pair(402, "Payment Required"));				//
-	status_code.insert(std::make_pair(403, "Forbidden"));						//
-	status_code.insert(std::make_pair(404, "Not Found"));						// 
-	status_code.insert(std::make_pair(405, "Method Not Allowed"));				// Method 권한이 없을 때
-	status_code.insert(std::make_pair(406, "Not Acceptable"));					//
-	status_code.insert(std::make_pair(407, "Proxy Authentication Required"));	//
-	status_code.insert(std::make_pair(408, "Request Timeout"));					//
-	status_code.insert(std::make_pair(409, "Conflict"));						//
-	status_code.insert(std::make_pair(410, "Gone"));							// 해당 html 파일이 존재하지 않을 때
-	status_code.insert(std::make_pair(411, "Length Required"));					//
-	status_code.insert(std::make_pair(412, "Precondition Failed"));				//
-	status_code.insert(std::make_pair(413, "Payload Too Large"));				//
-	status_code.insert(std::make_pair(414, "URI Too Long"));					//
-	status_code.insert(std::make_pair(415, "Unsupported Media Type"));			//
-	status_code.insert(std::make_pair(416, "Range Not Satisfiable"));			//
-	status_code.insert(std::make_pair(417, "Expectation Failed"));				//
-	status_code.insert(std::make_pair(426, "Upgrade Required"));				//
-	status_code.insert(std::make_pair(500, "Internal Server Error"));			//
-	status_code.insert(std::make_pair(501, "Not Implemented"));					//
-	status_code.insert(std::make_pair(502, "Bad Gateway"));						//
-	status_code.insert(std::make_pair(503, "Service Unavailable"));				//
-	status_code.insert(std::make_pair(504, "Gateway Timeout"));					//
-	status_code.insert(std::make_pair(505, "HTTP Version Not Supported"));		// HTTP version이 1.1이 아닐 때
+	switch (request.status_code)
+	{
+		case 200:
+			response.header = "200 OK HTTP/1.1"
+			;
+		case 404:
+			response.header = "404 NotFound"
+			response.url = "./www/404.html";
+		case 300:
+			response.header = "300 Redirection"
+			response.url = "./www/300.html";
+		...
+		default:
+			;
+	}
 }
+
+method
+
+POST -> 
+DELETE -> statuscode = 400;
+GET -> pass
+
+
+void response(Client client, Request request)
+{
+	Response	response;
+
+
+
+	if (request.status_code)
+		set_body();
+	else
+	{
+		if (request.is_cgi)
+			// cgi part (무엇을 보내주는가)
+		else if (request.is_directory)
+		{
+			std::string check_route = request.location->root + request.location->route + request.url.substr(0, request.url.find('?', 0));
+			std::ifstream test_fs;
+			test_fs.open(check_route);
+			if (!test_fs.is_open())
+				response.status_code = 404;
+			test_fs.close();
+			else
+			{
+				for (std::vector<std::string>::iterator iter = request.location->v_index.begin(); iter != request.location->v_index.end(); iter++)
+				{
+					std::string index_check = check_route + *iter;
+					test_fs.open(index_check)
+					if (test_fs.is_open())
+					{
+						response.status_code = 200;
+						response.url = index_check;
+						test_fs.close();
+						break ;
+					}
+					test_fs.close();
+				}
+				if (response.status_code == 0 && request.location->autoindex)
+				{
+					// autoindex 파트 --> CGI 로?
+					// 작성
+				}
+				else if (response.status_code == 0)
+					response.status_code = 404;
+			}
+		}
+		else
+		{
+			std::string check_route = request.url.substr(0, request.url.find('?', 0));
+			std::ifstream test_fs;
+			fs.open(check_route)
+			if (!test_fs.is_open())
+				response.status_code = 404;
+			else
+			{
+				response.status_code = 200;
+				response.url = check_route;
+			}
+			fs.close();
+		}
+	}
+	set_url(Request request, Response response);
+	//FILE 로 body 받아오기
+
+	file_path  -->  string  --> body;
+	client.respond_msg = response.header + "\r\n" + response.body;
+}
+
+/*
+	Question
+
+	redirection 은 프로세스가 어떻게 진행되는가
+	status_code를 저장할 곳이 필요한데, 어디가 좋을까 
+	directory일때 + CGI가 아닌 파일일때 똑같이 open 검증하는 과정이 들어가는데 이것을 합치는 방향(간결하게) 고민중
+	내일까지  set_url 함수 완성 예정
+
+	POST는 어디서 처리하는가
+*/
